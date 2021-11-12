f53fb114eec8befcf2ef8cfcca410d4495c3cc3f
ConcreteDunkey <concretedunkey@gmail.com>
2021-11-11 22:20:45 -0600

    Added oracle; Solr closer to working

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import ast
 import random
-import nltk, re, pprint
-from nltk import word_tokenize
+import nltk
+import re
+import pprint
 from rake_nltk import Rake
 import pysolr
 from nltk.stem import WordNetLemmatizer
 # from pywsd.lesk import simple_lesk  # Broken ?
 # Maybe.... ? from https://github.com/alvations/pywsd/blob/master/pywsd/utils.py#L129
 from pathlib import Path
+
+
+SOLR_CORE = 'jim_core'
 
 
 def get_wn_pos(tag):
@@ -25,65 +29,10 @@ def get_wn_pos(tag):
     return res
 
 
-def test():
-    articles_folder = Path('articles')
-    if not articles_folder.is_dir():
-        print("Articles directory not found. Aborting.")
-        return
-    article_files = articles_folder.glob('*.txt')
-    articles_content = {}
-    for i, file in enumerate(article_files):    # Debugging; load only one file
-        # if file.stem in ['111', '181', '199', '226', '247', '273', '282', '304', '390', '400', '428', '58', '6']:
-        #     continue
-        # print(file)
-        with open(file, encoding='utf-8') as f:
-            articles_content[file.stem] = f.readlines()
-    #
-    # solr_core = 'jim_core'
-    # solr = pysolr.Solr('http://localhost:8983/solr/' + solr_core, always_commit=True, timeout=10)
-    #
-    # for article in articles_content:
-    #     solr.add([{
-    #         'id': article,
-    #         'contents': articles_content[article]
-    #     }])
-    solr = load_articles()
-
-    results = solr.search('contents:"rebels"')
-
-    print("Saw {0} result(s).".format(len(results)))
-
-    for result in results:
-        print("The article id is '{0}'.".format(result['id']))
-
-    data = []
-    with open('data.txt', encoding='utf-8') as f:
-        for line in f.readlines():
-            data.append(line)
-    # a is a list of words in one article
-
-    a = articles_content[list(articles_content.keys())[0]][0]
-    tokens = word_tokenize(a)
-    # b is a, with POS tags
-    b = nltk.pos_tag(tokens)
-    # fd = nltk.FreqDist(tag for (word, tag) in b)
-    # print(fd.most_common())
-    # word_fd = nltk.FreqDist(b)
-    # c = [wt[0] for (wt, _) in word_fd.most_common() if wt[1] == 'NNP']
-    # print(c)
-    word_net_lemmatizer = WordNetLemmatizer()
-    # c = [word_net_lemmatizer.lemmatize(w, get_wn_pos(w)) for w in a]
-
-    # c is lemmatized set of words in list a
-    c = []
-    for w in b:
-        a = get_wn_pos(w[1])
-        if a != '':
-            c.append(word_net_lemmatizer.lemmatize(w[0], get_wn_pos(w[1])))
-        else:
-            c.append(w[0])
-    a = 4
-    # article = articles_content[articles_content.keys()[0]]
+def start_solr():
+    solr_core = SOLR_CORE
+    solr = pysolr.Solr('http://localhost:8983/solr/' + solr_core, always_commit=True, timeout=10)
+    return solr
 
 
 def load_articles():
@@ -95,17 +44,35 @@ def load_articles():
     articles_content = {}
     for i, file in enumerate(article_files):
         with open(file, encoding='utf-8') as f:
-            articles_content[file.stem] = f.readlines()
+            content = f.readlines()
+            articles_content[file.stem] = ' '.join(content)
+    return articles_content
 
-    solr_core = 'jim_core'
-    solr = pysolr.Solr('http://localhost:8983/solr/' + solr_core, always_commit=True, timeout=10)
 
+def load_solr(solr):
+    articles_content = load_articles()
     for article in articles_content:
+        art_pipelined = pipeline(article)
         solr.add([{
             'id': article,
             'contents': articles_content[article]
         }])
-    return solr
+
+
+def pipeline(article):
+    sentences = nltk.sent_tokenize(article)
+    tokens = nltk.word_tokenize(article)
+    tagged_text = nltk.pos_tag(tokens)
+    word_net_lemmatizer = WordNetLemmatizer()
+    lemmatized_text = []
+    for w in tagged_text:
+        a = get_wn_pos(w[1])
+        if a != '':
+            lemmatized_text.append(word_net_lemmatizer.lemmatize(w[0], get_wn_pos(w[1])))
+        else:
+            lemmatized_text.append(w[0])
+    a = 6
+    return a
 
 
 def import_q_a(q_a_file):
@@ -131,6 +98,18 @@ def test_q_a(q_a_dict, num_q, seed):
     return test_q_a_list
 
 
+def all_q_a(q_a_dict):
+    source_articles = q_a_dict.keys()
+    q_a_list = []
+    for source in source_articles:
+        num_src_q = len(q_a_dict[source])
+        for q_num in range(num_src_q):
+            q_a_list.append({'q': q_a_dict[source][q_num][0],
+                             'article': source,
+                             'a': q_a_dict[source][q_num][1]})
+    return q_a_list
+
+
 def get_keywords(question):
     rake = Rake()
     rake.extract_keywords_from_text(question)
@@ -140,10 +119,13 @@ def get_keywords(question):
 
 def answer_question(question, solr):
     keywords = get_keywords(question)
-    results = solr.search(f'contents:"{0}"'.format(' '.join(keywords)))
-    # print("Saw {0} result(s).".format(len(results)))
-    # for result in results:
-    #     print("The article id is '{0}'.".format(result['id']))
+    # keywords = ['Canadian', 'Alberta']
+    prefix = 'contents:"'
+    postfix = '"'
+    infix = '" OR contents:"'
+    keyword_string = prefix + infix.join(keywords) + postfix
+    query = keyword_string
+    results = solr.search(query)
     return results
 
 
@@ -156,10 +138,40 @@ def answer_questions(questions, solr):
         print(f"Expected article {question['article']}, got article {' '.join(res_list)}")
 
 
-if __name__ == '__main__':
-    index = load_articles()
+def oracle(q_a):
+    articles = load_articles()
+    zero_answers = 0
+    multi_answers = 0
+    for article in articles:
+        a = articles[article]
+        article_sent = nltk.sent_tokenize(articles[article])
+        for i, question in enumerate(q_a):
+            if str(question['article']) != article:
+                continue
+            count = a.count(question['a'])
+            if count == 1:
+                q_a[i]['o'] = [s for s in article_sent if question['a'] in s]
+            elif count == 0:
+                zero_answers += 1
+            else:
+                q_a[i]['o'] = [s for s in article_sent if question['a'] in s]
+                multi_answers += 1
+    return q_a
+
+
+def test():
+    index = start_solr()
+    articles_not_loaded = False  # TODO Write something to determine if articles are loaded
+    if articles_not_loaded:
+        load_articles(index)
     q_a = import_q_a('data.txt')
-    test_questions = test_q_a(q_a, num_q=5, seed=0)
-    answer_questions(test_questions, index)
+    # test_questions = test_q_a(q_a, num_q=5, seed=0)
+    all_questions = all_q_a(q_a)
+    all_answers = oracle(all_questions)
+    # answer_questions(test_questions, index)
     a = 5
     # test()
+
+
+if __name__ == '__main__':
+    test()
