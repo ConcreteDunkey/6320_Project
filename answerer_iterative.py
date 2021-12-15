59594ca75b808fe54b466eeb2477a9921a95f07f
ConcreteDunkey <concretedunkey@gmail.com>
2021-12-15 16:03:34 -0600

    iter_lasso dev

--- answerer_iterative.py
+++ answerer_iterative.py
@@ -105,9 +105,16 @@ def key_huer(question):
     huer_keys.append(find_clause_root_and_mods(tree, 'pobj', keep_np_pos, stop_np_pos))
     # Level 7 (verbs)
     huer_keys.append(find_clause_root_and_mods(tree, 'ROOT', keep_vp_pos, stop_vp_pos))
-    # # Level 8 (focus)
-    # huer_keys.append(find_clause_root_and_mods(tree, 'pobj', keep_np_pos, stop_np_pos))
+    # Level 8 (verbs2)
+    keep_vp_pos2 = ["VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    root_verbs = find_clause_root_and_mods(tree, 'ROOT', keep_vp_pos, stop_vp_pos)
+    get_synonyms(root_verbs)
     return huer_keys
+
+
+def get_synonyms(words):
+    # get hyper/hypo/tropo
+    return 7  # TODO
 
 
 def new_keywords(keys_so_far, maybe_new_keys):
@@ -223,7 +230,8 @@ class SimpleLassoAnswerer(Answerer):
         # if len(keywords) > 0:
         #     print(question)
         #     print(keywords)
-        query = make_and_query(keywords)
+        query = make_or_query(keywords)
+        # query = make_and_query(keywords)
         results = self.def_search(query)
         # results = {'docs': []}  # For debugging; return empty
         scores = []
@@ -272,89 +280,122 @@ def delta_scores(scores):
     return res
 
 
+l_scores = {2: {'t_mean':5.22, 't_sd':2.92, 'f_mean':0.36, 'f_sd':1.23, 'acc':380},
+            3: {'t_mean':6.22, 't_sd':3.14, 'f_mean':0.52, 'f_sd':1.44, 'acc':411},
+            6: {'t_mean':7.62, 't_sd':3.32, 'f_mean':0.22, 'f_sd':0.99, 'acc':476},
+            7: {'t_mean':7.31, 't_sd':4.48, 'f_mean':0.08, 'f_sd':0.58, 'acc':22},
+            8: {'t_mean':8.03, 't_sd':2.85, 'f_mean':5.32, 'f_sd':1.47, 'acc':983},
+            }
+
+
 class CompoundLassoAnswerer(Answerer):
-    # def __init__(self, solr, max_level=6, levels=None):
     def __init__(self, solr, max_level=6):
         super().__init__(solr)
-        # self.simple_lasso_levels = levels
         self.simple_lasso_level = max_level
-        # if levels is None:
-        #     self.keyword_method = lasso_keywords
-        # else:
-        #     self.keyword_method = lasso_keywords_alt
+        self.all_keywords = None
 
-    # @classmethod
-    # def basic(cls, solr):
-    #     return cls(solr)
-    #
-    # @classmethod
-    # def single_lvl(cls, solr, max_level):
-    #     return cls(solr, max_level)
-    #
-    # @classmethod
-    # def single_l1(cls, solr):
-    #     return cls(solr, max_level=1)
-    #
-    # @classmethod
-    # def single_l2(cls, solr):
-    #     return cls(solr, max_level=2)
-    #
-    # @classmethod
-    # def single_l3(cls, solr):
-    #     return cls(solr, max_level=3)
-    #
-    # @classmethod
-    # def single_l4(cls, solr):
-    #     return cls(solr, max_level=4)
-    #
-    # @classmethod
-    # def single_l5(cls, solr):
-    #     return cls(solr, max_level=5)
-    #
-    # @classmethod
-    # def single_l6(cls, solr):
-    #     return cls(solr, max_level=6)
-    #
-    # @classmethod
-    # def single_l7(cls, solr):
-    #     return cls(solr, max_level=7)
-    #
-    # @classmethod
-    # def many_lvl(cls, solr, levels):
-    #     return cls(solr, levels=levels)
-    #
-    # def this_keyword_grabber(self, question):
-    #     if self.simple_lasso_levels is None:
-    #         return self.keyword_method(question, self.simple_lasso_level)
-    #     else:
-    #         return self.keyword_method(question, self.simple_lasso_levels)
+    def modular_query(self, level, search_type):
+        flag = None  # flag gives directions on how to change the search
+                     # +1 if search was too narrow
+                     # -1 if search was too broad
+                     #  0 if search was good
+        standardized_score = -10
+        keywords = condense_lasso_keywords(self.all_keywords, level)
+        if search_type == 'and':
+            query = make_and_query(keywords)
+        elif search_type == 'or':
+            query = make_or_query(keywords)
+        else:                                 # Catch error
+            query = make_and_query(keywords)
+        results = self.def_search(query)
+        scores = []
+        for res in results.docs[0:20]:
+            scores.append(res['score'])
+        if len(results.docs) > 0:
+            standardized_score = (scores[0] - l_scores[level]['t_mean']) / l_scores[level]['t_sd']
+            if standardized_score > 2:
+                flag = 0
+            elif len(results.docs) > 1 and standardized_score > 0 and scores[1] - scores[0] < 1.5:
+                flag = 1
+            else:
+                flag = -1
+        else:
+            flag = 1
+        return results, scores, standardized_score, flag
 
     def this_method(self, question):
-        all_keywords = key_huer(question)
+        self.all_keywords = key_huer(question)
         flag = 0
+        level = 6
+        best_st_score = -11    # This is super janky; the search method gives minimum score -10
+        best_results = None
+        best_scores = None
         while 1 == 1:
-            keywords = condense_lasso_keywords(all_keywords, 6)
-            query = make_and_query(keywords)
-            results = self.def_search(query)
-            scores = []
-            for res in results.docs[0:20]:
-                scores.append(res['score'])
-            if len(scores) > 0:
-                if scores[0] > 10:
-                    flag = 1
+            results, scores, st_score, direction = self.modular_query(level, 'and')
+            if direction == 0:
+                flag = level
+                break
+            else:
+                if st_score > best_st_score:
+                    best_st_score = st_score
+                    best_results = results
+                    best_scores = scores
+            if direction > 0:
+                level = 7
+                results, scores, st_score, direction = self.modular_query(level, 'and')
+                if direction == 0:
+                    flag = level
                     break
-                # a = scores[0]
-                # b = delta_scores(scores)
-                # if len(scores)>1 and scores[0] > 7.5 and delta_scores(scores) > 4.5:
-                #     flag = 2
-                #     break
-            # if len(scores) > 0 and scores[0] > 2:
-            #     break
-            scores = []
-            for res in results.docs[0:2]:
-                scores.append(res['score'])
-            results = []
+                else:  # we didn't find a good solution
+                    if st_score > best_st_score:
+                        best_st_score = st_score
+                        best_results = results
+                        best_scores = scores
+                if direction > 0:  # we can relax search query
+                    level = 7
+                    results, scores, st_score, direction = self.modular_query(level, 'or')
+                    if direction == 0:
+                        flag = level
+                        break
+                    else:
+                        if st_score > best_st_score:
+                            best_st_score = st_score
+                            best_results = results
+                            best_scores = scores
+                else:  # we tired our best
+                    scores = best_scores
+                    results = best_results
+            else:  # Drop a level from level 6 (tighten query)
+                level = 3
+                results, scores, st_score, direction = self.modular_query(level, 'and')
+                if direction == 0:
+                    flag = level
+                    break
+                else:  # we didn't find a good solution
+                    if st_score > best_st_score:
+                        best_st_score = st_score
+                        best_results = results
+                        best_scores = scores
+                if direction > 0:  # we tried our best
+                    scores = best_scores
+                    results = best_results
+                else:  # Drop a level from level 3 (tighten query)
+                    level = 2
+                    results, scores, st_score, direction = self.modular_query(level, 'and')
+                    if direction == 0:
+                        flag = level
+                        break
+                    else:  # we didn't find a good solution
+                        if st_score > best_st_score:
+                            best_st_score = st_score
+                            best_results = results
+                            best_scores = scores
+            scores = best_scores
+            results = best_results
             break
+        for res in results.docs[0:2]:
+            scores.append(res['score'])
+        # results = []
         return results, scores, flag
 
     def answer(self, question):
